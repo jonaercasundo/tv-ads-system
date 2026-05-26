98341b32b7d6a01f995460da5af53851d0029048
Jonaer Casundo <jcasundo.sedge@gmail.com>
2026-05-26 12:00:42 +0800

    updated tvAds

--- routes/web.php
+++ routes/web.php
@@ -4,25 +4,47 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\VideoController;
 use App\Http\Controllers\AuthController;
 
-Route::get('/api/videos', [VideoController::class, 'index']);
-Route::post('/api/videos', [VideoController::class, 'store']);
-Route::delete('/api/videos/{id}', [VideoController::class, 'destroy']);
-Route::get('/admin', function () {
-    return view('admin');
-});
-Route::delete('/api/videos/{id}', [VideoController::class, 'destroy']);
-Route::post('/api/videos/upload-zip', [VideoController::class, 'uploadZip']);
+/*
+|----------------------------------
+| TV DISPLAY (DEFAULT SCREEN)
+|----------------------------------
+*/
 Route::get('/', function () {
     return view('tv');
 });
+
+Route::get('/tv', function () {
+    return view('tv');
+});
+
+/*
+|----------------------------------
+| AUTH
+|----------------------------------
+*/
 Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
 Route::get('/logout', [AuthController::class, 'logout']);
-Route::get('/admin', function () {
 
+/*
+|----------------------------------
+| ADMIN (PROTECTED)
+|----------------------------------
+*/
+Route::get('/admin', function () {
     if (!session('admin_logged_in')) {
         return redirect('/login');
     }
 
     return view('admin');
 });
+
+/*
+|----------------------------------
+| API
+|----------------------------------
+*/
+Route::get('/api/videos', [VideoController::class, 'index']);
+Route::post('/api/videos', [VideoController::class, 'store']);
+Route::post('/api/videos/upload-zip', [VideoController::class, 'uploadZip']);
+Route::delete('/api/videos/{id}', [VideoController::class, 'destroy']);
